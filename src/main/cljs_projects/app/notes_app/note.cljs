41236fe26ff161d72804de559ffe8d6c8ec073c6
Moro-Afriyie <ceremoinc99@gmail.com>
2025-04-02 10:31:33 +0000

    add the notes modal

--- src/main/cljs_projects/app/notes_app/note.cljs
+++ src/main/cljs_projects/app/notes_app/note.cljs
@@ -8,14 +8,14 @@
     (.toLocaleDateString date "en-US" #js {:year "numeric" :month "long" :day "numeric"})))
 
 
-(mf/defc note [{:keys [id title content date]}]
+(mf/defc note [{:keys [id title description date]}]
   [:div {:class "w-70 min-h-70  p-4 flex  flex-col gap-2 border border-yellow-300 text-4xl rounded-xl  bg-yellow-100"  :key id}
    [:h6 {:class "text-center text-2xl font-bold capitalize"} title]
    [:div {:class "text-left text-xs flex-grow"}
-    [:p {:class ""}  content]]
+    [:p {:class ""}  description]]
    [:div {:class "text-xs flex justify-between items-center"}
     [:div {:class ""}
      [:span {:class ""} (format-date date)]]
-    [:div {:class "flex gap-4 item-center"}
+    [:div {:class "flex gap-6 item-center"}
      [:i {:class "fa-solid fa-pen-to-square cursor-pointer" :style {:font-size "1rem"}}]
      [:i {:class "fa-solid fa-trash cursor-pointer" :style {:font-size "1rem"}}]]]])
